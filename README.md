2d4f9838e54e590ba8a658183931f10471a0e0b4
Ansu John <ansu.john@sap.com>
2021-01-06 18:09:17 +0000

    With tests

--- README.md
+++ README.md
@@ -13,4 +13,12 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+
 # cdk-workshop
+
+CDK creates
+- API Gateway REST endpoint
+- Lambda functions
+- DynamoDB
+- Exposes a public HTTP endpoint which displays an HTML page with the contents of a DynamoDB table
--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,18 +1,42 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import { Code, Function as LambdaFunction, Runtime } from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
+
+import { HitCounter } from './hitCounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const hello = new LambdaFunction(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_10_X,
+      code: Code.fromAsset('lambda'),
+      handler: 'hello.handler'
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    const restAPI = new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+
+    //Output API's URL
+    new cdk.CfnOutput(this, 'APIUrl', {
+      value: `https://${restAPI.restApiId}.execute-api.${this.region}.amazonaws.com/prod/`,
+    });
+
+    cdk.Tags.of(this).add('cdk-workshop', 'Y');
+
+    //exposes a public HTTP endpoint which displays an HTML page with the contents of a DynamoDB table in your stack.
+    //not for PROD, only for dev purposes
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits' 
+    });
   }
 }
