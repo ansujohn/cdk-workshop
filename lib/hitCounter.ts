a5010901eff498db9389d2cbc0cc39db30bf17ae
Ansu John <ansu.john@sap.com>
2021-01-06 18:14:17 +0000

    With tests

--- lib/hitCounter.ts
+++ lib/hitCounter.ts
@@ -3,42 +3,57 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
-  /** the function for which we want to count url hits **/
-  downstream: lambda.Function;
+    /** the function for which we want to count url hits **/
+    downstream: lambda.Function;
+
+    /**
+    * The read capacity units for the table
+    *
+    * Must be greater than 5 and lower than 20
+    *
+    * @default 5
+    */
+    readCapacity?: number;
 }
 
 export class HitCounter extends cdk.Construct {
 
-  /** allows accessing the counter function */
-  public readonly handler: lambda.Function;
+    /** allows accessing the counter function */
+    public readonly handler: lambda.Function;
 
-  /** the hit counter table */
-  public readonly table: dynamodb.Table;
+    /** the hit counter table */
+    public readonly table: dynamodb.Table;
 
-  constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
-    super(scope, id);
+    constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
 
-    this.table = new dynamodb.Table(this, 'Hits', {
-        partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING },
-        removalPolicy: cdk.RemovalPolicy.DESTROY,
-        serverSideEncryption: true,
-    });
+        if (props.readCapacity !== undefined && (props.readCapacity < 5 || props.readCapacity > 20)) {
+            throw new Error('readCapacity must be greater than 5 and lower than 20');
+        }
+
+        super(scope, id);
+
+        this.table = new dynamodb.Table(this, 'Hits', {
+            partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING },
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            serverSideEncryption: true,
+            readCapacity: props.readCapacity ?? 5
+        });
 
 
-    this.handler = new lambda.Function(this, 'HitCounterHandler', {
-      runtime: lambda.Runtime.NODEJS_10_X,
-      handler: 'hitcounter.handler',
-      code: lambda.Code.fromAsset('lambda'),
-      environment: {
-        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-        HITS_TABLE_NAME: this.table.tableName
-      }
-    });
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_10_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: this.table.tableName
+            }
+        });
 
-    // grant the lambda role read/write permissions to our table
-    this.table.grantReadWriteData(this.handler);
+        // grant the lambda role read/write permissions to our table
+        this.table.grantReadWriteData(this.handler);
 
-    // grant the lambda role invoke permissions to the downstream function
-    props.downstream.grantInvoke(this.handler);
-  }
+        // grant the lambda role invoke permissions to the downstream function
+        props.downstream.grantInvoke(this.handler);
+    }
 }
